c1b1e9a6708d4f3e637066093105371251350f05
kanan-droid <kananghanbri98@gmail.com>
2025-02-09 16:20:53 +0100

    this is where i finished the program Version 1.0

--- main.go
+++ main.go
@@ -7,12 +7,16 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type Task struct {
-	ID     int    `json:"id"`
-	Name   string `json:"name"`
-	IsDone bool   `json:"completed"`
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	IsDone      bool      `json:"completed"`
+	AdedDate    time.Time `json:"addedtime"`
+	Description string    `json:"description"`
+	Started     bool      `json:"taskstartedornot"`
 }
 
 var Taskslist []Task
@@ -44,35 +48,57 @@ func SavingThetasks() {
 }
 
 // add task here
-func AddNEwTask(nameOfthetask string) {
+func AddNEwTask(nameOfthetask string, desc string) {
 	taskId := len(Taskslist) + 1
-	task := Task{ID: taskId, Name: nameOfthetask, IsDone: false}
+	task := Task{ID: taskId, Name: nameOfthetask, IsDone: false, AdedDate: time.Now(), Description: desc, Started: false}
 	Taskslist = append(Taskslist, task)
 	SavingThetasks()
 	fmt.Print("task added ", nameOfthetask)
 }
 
 // this one shows Tasks
-func showAllTask(mmd bool) {
+func showAllTask(onlyuncompe bool, onlyInprogress bool) {
 	if len(Taskslist) == 0 {
 		fmt.Print("no task avalible")
 		return
 	}
-	//true uncompele
+	var counting bool = true
 	for _, task1 := range Taskslist {
 		maybyitsdone := "pending"
-
-		if task1.IsDone {
-			maybyitsdone = "task done"
-			if mmd {
+		if onlyInprogress {
+			//this if make sure if task has not been started dont show it or if task is finished dont show it show only the one that are in progerssion
+			if !task1.Started || task1.IsDone {
 				continue
 			}
 		}
 
-		fmt.Printf("\n%d. %s - %s \n", task1.ID, task1.Name, maybyitsdone)
+		if task1.IsDone {
+			//this is to show only the unvompleted tasks and skip the for loop
+			if onlyuncompe {
+				continue
+			}
+			maybyitsdone = "task done"
+			//for uncomplete task
+		}
+		if task1.Started {
+			maybyitsdone = "in progress"
 
+		}
+		//time management
+		Timebeforcreation := time.Since(task1.AdedDate)
+		var ageago string
+		if Timebeforcreation > 24 {
+			ageago = fmt.Sprintf("sinc %f hours ago", Timebeforcreation.Hours())
+		} else {
+			ageago = fmt.Sprintf("sinc %f days ago", Timebeforcreation.Hours()/24)
+		}
+
+		fmt.Printf("\n%d.%s - %s \ndescerption:%s -Started:%s \n", task1.ID, task1.Name, maybyitsdone, task1.Description, ageago)
+		counting = false
 	}
-
+	if counting {
+		fmt.Print("\n No uncompleted task available")
+	}
 }
 
 // remove tasks
@@ -105,78 +131,107 @@ func Taskremover(ID string) {
 }
 
 // this func mark tasks as done
-func MarkingTaskDone(Id string) {
+func MarkingTaskDone(Id string, markingitdone bool, startingIt bool) {
 	if len(Taskslist) == 0 {
 		fmt.Print("no task available")
 		return
 	}
-
+	//input from user to intiger
 	taskID, err := strconv.Atoi(strings.TrimSpace(Id))
 	if err != nil {
 		fmt.Print("invalid task ID")
 		return
 	}
 	var updatTasks []Task
-	for _, task := range Taskslist {
-		if task.ID == taskID {
-			task.IsDone = true
+
+	for i := range Taskslist {
+		if Taskslist[i].ID == taskID {
+			if startingIt {
+				Taskslist[i].Started = true
+			}
+			if markingitdone {
+				Taskslist[i].IsDone = true
+			}
+			break
 		}
-		updatTasks = append(updatTasks, task)
 	}
+
+	fmt.Printf("\nTask %d has been updated", taskID)
 	Taskslist = updatTasks
 	SavingThetasks()
-	fmt.Printf("\nTask %d Marked as Done\n", taskID)
+
 }
 
 // main loop with everything
 func main() {
 	gettingAllTasks()
-	fmt.Print("Welcome")
+	fmt.Print("Welcome\n")
 	for {
-		fmt.Print("\n1.Add Task \n2.Show Tasks\n3.Mark Task as Done\n4.Delete Task\n5.Exite\n")
+		fmt.Print("\n1. Add Task \n2. Show Tasks\n3.Update task\n4. Delete Task\n5.Exit\n")
 
 		var choices int
 		fmt.Scan(&choices)
-
+		reader := bufio.NewReader(os.Stdin)
+		reader.ReadString('\n')
 		switch choices {
-		//add task
+
 		case 1:
 			reader := bufio.NewReader(os.Stdin)
 
-			fmt.Print("\nEnterTaskName :")
-			reader.ReadLine()
+			fmt.Print("\nEnter Task Name: ")
 			name, _ := reader.ReadString('\n')
-			AddNEwTask(name)
+			name = strings.TrimSpace(name)
 
-			//show task
+			fmt.Print("\nEnter Task Description: ")
+			description, _ := reader.ReadString('\n')
+			description = strings.TrimSpace(description)
+
+			AddNEwTask(name, description)
+
 		case 2:
-
 			userinput1 := bufio.NewReader(os.Stdin)
-			fmt.Print("1.Show only uncompleted task\n2.show all of the tasks\n==:")
-			userinput1.ReadLine()
+			fmt.Print("1. Show only uncompleted tasks\n2. Show all tasks\n ")
 			text, _ := userinput1.ReadString('\n')
-			onlycomp := false
-			if text == "1" {
-				onlycomp = true
-			}
-			showAllTask(onlycomp)
+			text = strings.TrimSpace(text)
 
-			//mark task as done
+			uncomp := false
+			if text == "1" {
+				uncomp = true
+			}
+			showAllTask(uncomp, false)
+
+		// Update Task
 		case 3:
-			showAllTask(false)
-			fmt.Print("\nEnter taskID :")
-			userinput3 := bufio.NewReader(os.Stdin)
-			userinput3.ReadLine()
-			text, _ := userinput3.ReadString('\n')
-			MarkingTaskDone(text)
-			//delete task
+			userinput1 := bufio.NewReader(os.Stdin)
+			fmt.Print("\n1.start a task \n2.mark task done\n")
+			text, _ := userinput1.ReadString('\n')
+
+			if text == "1" {
+				showAllTask(true, false)
+				fmt.Print("\nwhich task would you like to Start?\n")
+			}
+			if text == "2" {
+				showAllTask(false, true)
+				fmt.Print("\nwhich task would you like to Mark Done?\n")
+			}
+			userinput4 := bufio.NewReader(os.Stdin)
+			text423, _ := userinput4.ReadString('\n')
+			if text == "1" {
+				MarkingTaskDone(text423, false, true)
+			} else {
+				MarkingTaskDone(text423, true, false)
+			}
+
+		// Delete Task
 		case 4:
-			showAllTask(false)
-			fmt.Print("\nEnter taskID :")
+			showAllTask(false, false)
+			fmt.Print("\nEnter Task ID: ")
 			userinput2 := bufio.NewReader(os.Stdin)
-			userinput2.ReadLine()
 			text, _ := userinput2.ReadString('\n')
+			text = strings.TrimSpace(text)
 			Taskremover(text)
+
+		// Exit
 		case 5:
 			os.Exit(0)
 		}
